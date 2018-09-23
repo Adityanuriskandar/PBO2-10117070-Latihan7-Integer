3099a72a1041ce8be6d0bf692b6774ef59c9c47c
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:13:22 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan7-Integer/src/pbo2/pkg10117070/latihan7/integer/PBO210117070Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan7.integer;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk Mengetahui 
+ * jenis - jenis integer.
+ */
+public class PBO210117070Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte\t: " + b);
+        System.out.println("short\t: " + s);
+        System.out.println("int\t: " + i);
+        System.out.println("long\t: " + l);
+        
+    }
+    
+}
